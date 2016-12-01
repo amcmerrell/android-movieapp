ba6dabd38eeef443f23dc8b11ced2b2e89cab82f
Andrew Merrell and Yusuf Qedan <andrew+yusuf@>
2016-12-01 08:50:59 -0800

    add method to call Movie Database API.

---
+++ app/src/main/java/com/epicodus/movieapp/Constants.java
@@ -0,0 +1,11 @@
+package com.epicodus.movieapp;
+
+/**
+ * Created by Guest on 12/1/16.
+ */
+public class Constants {
+    public static final String MOVIE_DATABASE_API_KEY = BuildConfig.MOVIE_DATABASE_API_KEY;
+    public static final String MOVIE_DATABASE_BASE_URL = "https://api.themoviedb.org/3/search/movie?include_adult=false&language=en-US";
+    public static final String MOVIE_DATABASE_KEY_PARAMETER = "api_key";
+    public static final String MOVIE_DATABASE_TITLE_PARAMETER = "query";
+}
---
+++ app/src/main/java/com/epicodus/movieapp/MainActivity.java
@@ -0,0 +1,40 @@
+package com.epicodus.movieapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.Response;
+
+public class MainActivity extends AppCompatActivity {
+    public static final String TAG = MainActivity.class.getSimpleName();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        String title = "Fight Club";
+        getMovies(title);
+    }
+
+    private void getMovies(String title) {
+        final MovieService movieService = new MovieService();
+        movieService.findMovies(title, new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                e.printStackTrace();
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                String jsonData = response.body().string();
+                Log.v(TAG, jsonData);
+            }
+        });
+    }
+}
